0b19f3f55030202f41d84558a72ceea098c50377
Abdelrhman-Abdelaziz <85349614+Abdelrhman-Abdelaziz@users.noreply.github.com>
2022-03-19 16:01:46 +0200

    updata hotal and holder files

--- Hotel.java
+++ Hotel.java
@@ -3,82 +3,67 @@ import java.util.Scanner;
 
 class Hotel{
     static holder ob = new holder();
-    static Scanner sc = new Scanner(System.in);    
-    
+    static Scanner sc = new Scanner(System.in);
+
     //return rooms of specific room type
     public static SingleRoom[] rooms_of_roomType(int room_type){
-        SingleRoom[] temp = null;
+        SingleRoom[] temp;
         switch (room_type){
-                case 1 -> {
-                    temp=ob.drl;
-                }
-                case 2 -> {
-                    temp=ob.drd;
-                }
-                case 3 -> {
-                    temp=ob.srl;
-                }
-                case 4 -> {
-                    temp=ob.srd;  
-                }
-                default ->{
-                        return temp;
-                }
+            case 1 -> temp=ob.drl;
+            case 2 -> temp=ob.drd;
+            case 3 -> temp=ob.srl;
+            case 4 -> temp=ob.srd;
+            default ->{
+                return null;
+            }
         }
         return temp;
     }
     /*
     show available room and book room if available
-    */ 
+    */
     static void bookroom(int room_type){
-            int room_number;
-            int no_of_rooms=0,k=0;
-            SingleRoom[] temp=rooms_of_roomType(room_type);
-            switch (room_type){
-                case 1 -> {
-                    k=1;
-                }
-                case 2 -> {
-                    k=11;
-                }
-                case 3 -> {
-                    k=31;
-                }
-                case 4 -> { 
-                    k=41;  
-                }
-                default -> {
-                        System.out.println("Enter valid option");
-                        return;
-                }
-            }
-            System.out.println("Choose the room that you want to book: ");
-            no_of_rooms=temp.length; 
-            for(int i = 0;i < no_of_rooms; i++){
-                if(temp[i] == null){
-                    System.out.print(i+k+",");
-                }
-            }
-            System.out.print("\nEnter the room number that you want to select: ");
-            try{
-                room_number=sc.nextInt();
-                room_number=room_number-k;
-                if(temp[room_number] != null)
-                    throw new NotAvailable();
-
-                custDetail(room_type, room_number);
-            }
-            catch (NotAvailable e){
-                System.out.println(e.toString());
+        int room_number;
+        int no_of_rooms,k;
+        SingleRoom[] temp=rooms_of_roomType(room_type);
+        switch (room_type){
+            case 1 -> k=1;
+            case 2 -> k=11;
+            case 3 -> k=31;
+            case 4 -> k=41;
+            default -> {
+                System.out.println("Enter valid option");
                 return;
             }
-            catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("enter vaild option");
-                return;
-            }
-
-            System.out.println("Room Booked");
         }
+        System.out.println("Choose the room that you want to book: ");
+        assert temp != null;
+        no_of_rooms=temp.length;
+        for(int i = 0;i < no_of_rooms; i++){
+            if(temp[i] == null){
+                System.out.print(i+k+",");
+            }
+        }
+        System.out.print("\nEnter the room number that you want to select: ");
+        try{
+            room_number=sc.nextInt();
+            room_number=room_number-k;
+            if(temp[room_number] != null)
+                throw new NotAvailable();
+
+            custDetail(room_type, room_number);
+        }
+        catch (NotAvailable e){
+            System.out.println(e);
+            return;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("enter vaild option");
+            return;
+        }
+
+        System.out.println("Room Booked");
+    }
 
     //customer detalis
     /*
@@ -104,17 +89,12 @@ class Hotel{
             gender2 = sc.next();
         }
 
-        switch (room_type){
-            case 1: ob.drl[room_number] = new DoubleRoom(name, contact, gender, name2, contact2, gender2);
-                break;
-            case 2: ob.drd[room_number] = new DoubleRoom(name, contact, gender, name2, contact2, gender2);
-                break;
-            case 3: ob.srl[room_number] = new SingleRoom(name, contact, gender);
-                break;
-            case 4: ob.srd[room_number] = new SingleRoom(name, contact, gender);
-                break;
-            default:System.out.println("Wrong option");
-                break;
+        switch (room_type) {
+            case 1 -> ob.drl[room_number] = new DoubleRoom(name, contact, gender, name2, contact2, gender2);
+            case 2 -> ob.drd[room_number] = new DoubleRoom(name, contact, gender, name2, contact2, gender2);
+            case 3 -> ob.srl[room_number] = new SingleRoom(name, contact, gender);
+            case 4 -> ob.srd[room_number] = new SingleRoom(name, contact, gender);
+            default -> System.out.println("Wrong option");
         }
     }
 
@@ -122,7 +102,7 @@ class Hotel{
     show features of each room type
     */
     static void features(int room_type){
-        
+
         switch (room_type) {
             case 1 -> System.out.println("Number of double beds : 1\nAC : Yes\nFree breakfast : Yes\nCharge per day:5000 ");
             case 2 -> System.out.println("Number of double beds : 1\nAC : No\nFree breakfast : Yes\nCharge per day:3500  ");
@@ -131,7 +111,7 @@ class Hotel{
             default -> System.out.println("Enter valid option");
         }
     }
-    
+
     /*
     show number of available rooms for specific type
     */
@@ -150,38 +130,31 @@ class Hotel{
         }
         System.out.println("Number of rooms available : "+count);
     }
-    
+
     static void bill(int room_number, int room_type){
         double amount=0;
         String[] list={"Sandwich","Pasta","Noodles","Coke"};
         System.out.println("\n*******");
         System.out.println(" Bill:-");
         System.out.println("*******");
-        
+
         SingleRoom[] temp=rooms_of_roomType(room_type);
         switch (room_type){
-            case 1 -> {
-                amount += 5000;
-            }
-            case 2 -> {
-                amount+=3500;
-            }
-            case 3 -> {
-                amount+=2000;
-            }
-            case 4 -> {
-                amount+=1200;
-            }
+            case 1 -> amount += 5000;
+            case 2 -> amount+=3500;
+            case 3 -> amount+=2000;
+            case 4 -> amount+=1200;
             default ->{
-                    System.out.println("Not valid");
-                    return ;
-           }
-        }      
+                System.out.println("Not valid");
+                return ;
+            }
+        }
         System.out.println("Room Charge - "+amount);
         System.out.println("\nFood Charges:- ");
         System.out.println("===============");
         System.out.println("Item   Quantity    Price");
         System.out.println("-------------------------");
+        assert temp != null;
         for(Food bi:temp[room_number].food)
         {
             amount+= bi.getPrice();
@@ -190,42 +163,28 @@ class Hotel{
         }
         System.out.println("\nTotal Amount- "+amount);
     }
-   
+
     static void order(int room_number, int room_type){
-        int i, q;
-        char wish;
-        try{
+        // check that room booked or not
+        if(ob.getRoom(room_number,room_type) == null)
+        {
+            System.out.println("\nRoom not booked");
+        } else {
             System.out.println("\n==========\n   Menu:  \n==========\n\n1.Sandwich\tRs.100\n2.Pasta\t\tRs.130\n3.Noodles\tRs.150\n4.Coke\t\tRs.60\n");
+            char wish;
             do {
                 System.out.print("Enter your choice- ");
-                i=sc.nextInt();
+                int i = sc.nextInt();
                 System.out.print("Quantity- ");
-                q = sc.nextInt();
-                switch (room_type){
-                    case 1 -> {
-                        ob.drl[room_number].food.add(new Food(i, q));
-                        if(i > 4 || q <= 0)
-                            ob.drl[room_number].food.remove(ob.drl[room_number].food.size() - 1);
-                    }
-                    case 2 -> {
-                        ob.drd[room_number].food.add(new Food(i, q));
-                        if(i > 4 || q <= 0)
-                            ob.drd[room_number].food.remove(ob.drd[room_number].food.size() - 1);
-                    }
-                    case 3 -> {
-                        ob.srl[room_number].food.add(new Food(i, q));
-                        if(i > 4 || q <= 0)
-                            ob.srl[room_number].food.remove(ob.srl[room_number].food.size() - 1);
-                    }
-                    case 4 -> {
-                        ob.srd[room_number].food.add(new Food(i, q));
-                        if(i > 4 || q <= 0)
-                            ob.srd[room_number].food.remove(ob.srd[room_number].food.size() - 1);
-                    }
+                int q = sc.nextInt();
+                if(i <= 4 && q >= 0) {
+                    ob.getRoom(room_number,room_type).food.add(new Food(i, q));
+                }else {
+                    System.out.println("Invalid choice");
                 }
                 System.out.println("Do you want to order anything else ? (y/n)");
                 wish=sc.next().charAt(0);
-            }while (wish == 'y' || wish == 'Y');
+            } while (wish == 'y' || wish == 'Y');
 
             System.out.println("Do you want to see the bill ? (y/n)");
             wish = sc.next().charAt(0);
@@ -233,82 +192,24 @@ class Hotel{
                 bill(room_number, room_type);
             }
         }
-        catch (NullPointerException e){
-            System.out.println("\nRoom not booked");
-        }
-        catch (Exception e){
-            System.out.println("Cannot be done");
-        }
     }
 
     static void deallocate(int room_number, int room_type){
-        char w;
-        switch (room_type){
-            case 1 -> {
-                if(ob.drl[room_number] != null)
-                    System.out.println("This room is used by "+ ob.drl[room_number].getName());
-                else{
-                    System.out.println("Room is already empty!!");
-                    return;
-                }
-                System.out.println("Do you want to checkout ?(y/n)");
-                w=sc.next().charAt(0);
-                if(w=='y' || w == 'Y'){
-                    bill(room_number, room_type);
-                    ob.drl[room_number] =null;
-                    System.out.println("Room Deallocated succesfully");
-                }
-            }
-            case 2 -> {
-                if(ob.drd[room_number] != null)
-                    System.out.println("This room is used by "+ ob.drd[room_number].getName());
-                else{
-                    System.out.println("Room is already empty!!");
-                    return;
-                }
-                System.out.println("Do you want to checkout ?(y/n)");
-                w=sc.next().charAt(0);
-                if(w=='y' || w == 'Y'){
-                    bill(room_number, room_type);
-                    ob.drd[room_number] =null;
-                    System.out.println("Room Deallocated succesfully");
-                }
-            }
-
-            case 3 -> {
-                if(ob.srl[room_number] != null)
-                    System.out.println("This room is used by "+ ob.srl[room_number].getName());
-                else{
-                    System.out.println("Room is already empty!!");
-                    return;
-                }
-                System.out.println("Do you want to checkout ?(y/n)");
-                w=sc.next().charAt(0);
-                if(w=='y' || w == 'Y'){
-                    bill(room_number, room_type);
-                    ob.srl[room_number] =null;
-                    System.out.println("Room Deallocated succesfully");
-                }
-            }
-
-            case 4 -> {
-                if(ob.srd[room_number] != null)
-                    System.out.println("This room is used by "+ ob.srd[room_number].getName());
-                else{
-                    System.out.println("Room is already empty!!");
-                    return;
-                }
-                System.out.println("Do you want to checkout ?(y/n)");
-                w=sc.next().charAt(0);
-                if(w=='y' || w == 'Y'){
-                    bill(room_number, room_type);
-                    ob.srd[room_number] =null;
-                    System.out.println("Room Deallocated succesfully");
-                }
-            }
-            default -> System.out.println("\nEnter valid option : ");
+        if(ob.getRoom(room_number,room_type) == null)
+        {
+            System.out.println("Room is already empty!!");
         }
-
+        else {
+            char w;
+            System.out.println("This room is used by "+ ob.getRoom(room_number,room_type).getName());
+            System.out.println("Do you want to checkout ?(y/n)");
+            w=sc.next().charAt(0);
+            if(w=='y' || w == 'Y'){
+                bill(room_number, room_type);
+                ob.setRoom(room_number,room_type,null);
+                System.out.println("Room Deallocated successfully");
+            }
+        }
 
     }
 }
--- holder.java
+++ holder.java
@@ -7,4 +7,26 @@ class holder implements Serializable {
     DoubleRoom[] drd = new DoubleRoom[20]; // Deluxe Double Room
     SingleRoom[] srl = new SingleRoom[10]; // Luxury Single Room
     SingleRoom[] srd = new SingleRoom[20]; // Deluxe Single Room
+
+
+    public SingleRoom getRoom(int room_number, int room_type){
+        SingleRoom room;
+        switch (room_type) {
+            case 1 -> room = drl[room_number];
+            case 2 -> room = drd[room_number];
+            case 3 -> room = srl[room_number];
+            case 4 -> room = srd[room_number];
+            default -> room = null;
+        }
+        return room;
+    }
+    public void setRoom(int room_number, int room_type , SingleRoom room){
+        switch (room_type) {
+            case 1 -> drl[room_number] = (DoubleRoom) room;
+            case 2 -> drd[room_number] = (DoubleRoom) room;
+            case 3 -> srl[room_number] = room;
+            case 4 -> srd[room_number] = room;
+        }
+    }
+
 }
